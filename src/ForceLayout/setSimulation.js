8af16c2b8b3f24fb2ad2f4a1cca8aa665e5fc000
chiara-yen <chiara_yen@trendmicro.com>
2020-06-08 00:36:41 +0800

    refactor: extract ticked function

--- src/ForceLayout/setSimulation.js
+++ src/ForceLayout/setSimulation.js
@@ -1,17 +1,19 @@
 import * as d3 from "d3";
 import { svgConfig, nodeConfig } from "./configs";
 
+const ticked = (svg) => {
+  d3.select(svg)
+    .selectAll(".node")
+    .attr("transform", (d) => `translate(${d.x},${d.y})`);
+};
+
 const setSimulation = (svg, nodes) => {
   d3.forceSimulation()
     .force("charge", d3.forceManyBody())
     .force("center", d3.forceCenter(svgConfig.WIDTH / 2, svgConfig.HEIGHT / 2))
     .force("collision", d3.forceCollide(nodeConfig.CIRCLE_RADIUS))
     .nodes(nodes, (d) => d.id)
-    .on("tick", () => {
-      d3.select(svg)
-        .selectAll(".node")
-        .attr("transform", (d) => `translate(${d.x},${d.y})`);
-    });
+    .on("tick", () => ticked(svg));
 };
 
 export default setSimulation;
