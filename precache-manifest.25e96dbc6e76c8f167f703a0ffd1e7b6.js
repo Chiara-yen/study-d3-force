71aa6aa222e933a19375728bc3b2c5e382561c79
chiara-yen <chiara_yen@trendmicro.com>
2020-07-01 19:11:27 +0800

    Updates

---
+++ precache-manifest.25e96dbc6e76c8f167f703a0ffd1e7b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e2252b6e73dd2780520ac084e4e926",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b2abe95ace8f0fe4608",
+    "url": "/static/css/main.551adb16.chunk.css"
+  },
+  {
+    "revision": "c75fe167734db5b8dd91",
+    "url": "/static/js/2.fb29d699.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.fb29d699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2abe95ace8f0fe4608",
+    "url": "/static/js/main.b5438677.chunk.js"
+  },
+  {
+    "revision": "916174227b04c6a4c980",
+    "url": "/static/js/runtime-main.4c2c06a0.js"
+  }
+]);
